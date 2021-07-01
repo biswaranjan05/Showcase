b47a06d41de268202603ecb45226388e39fe793e
biswaranjan05 <1997biswa@gmail.com>
2021-07-01 14:03:39 +0530

    New Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { RouterModule } from '@angular/router';
+
 import { IonicModule } from '@ionic/angular';
 
-// import { CoreModule } from '@app/core';
-// import { SharedModule } from '@app/shared';
+
 import { HomeModule } from './home/home.module';
-import { ShellModule } from './shell/shell.module';
+
 import { AboutModule } from './about/about.module';
-// import { LoginModule } from './login/login.module';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NewComponentComponent } from './new-component/new-component.component';
@@ -20,16 +17,13 @@ import { NewComponentComponent } from './new-component/new-component.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    TranslateModule.forRoot(),
+    
     IonicModule.forRoot(),
-    // CoreModule,
-    // SharedModule,
-    ShellModule,
+    
     HomeModule,
     AboutModule,
-    // LoginModule,
-    AppRoutingModule // must be imported as the last module as it contains the fallback route
+    
+    AppRoutingModule 
   ],
   declarations: [AppComponent, NewComponentComponent],
   providers: [],
